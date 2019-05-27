225fe6513bb27d1717fe9eda500f920d64482c37
Iskra B <ibatistapoblete@gmail.com>
2019-05-27 05:11:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,28 +7,10 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
-    @songs = []
-    @genres = []
-  end
-
-  def save
-    @@all << self
   end
 
   def self.all
     @@all
-  end
-
-  def add_song(song)
-    @songs << song
-  end
-
-  def genres
-    @genres
-  end
-
-  def songs
-    @songs
   end
 
   def new_song(name, genre)
