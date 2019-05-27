830688eb34c792dbeed9d625839592b6fd173ffb
Iskra B <ibatistapoblete@gmail.com>
2019-05-27 05:41:44 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,4 +17,16 @@ class Patient
     new_appt
   end
 
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+
 end
